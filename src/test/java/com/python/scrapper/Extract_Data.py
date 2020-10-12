68c4eed00e3194380ac6006d7843ae59b01fcc55
Marwan Sultan <marwan.sultan@gmail.com>
2020-10-12 14:34:51 -0400

    first commit

---
+++ src/test/java/com/python/scrapper/Extract_Data.py
@@ -0,0 +1,10 @@
+import bs4 as bs
+import urllib.request
+
+
+data = urllib.request.urlopen('https://www.cnn.com/').read()
+
+soup = bs.BeautifulSoup(data, 'lxml')
+
+for link in soup.find_all('a'):
+    print(link.text)
